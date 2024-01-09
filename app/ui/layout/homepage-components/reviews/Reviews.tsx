a93cf54866b25df23e94033986cd70ab8bf29fe0
Lucas Amberg <102396588+lucas-amberg@users.noreply.github.com>
2024-01-09 14:42:11 -0800

    Create Reviews.tsx and import to homepage

---
+++ app/ui/layout/homepage-components/reviews/Reviews.tsx
@@ -0,0 +1,10 @@
+import HeaderDesc from "../../HeaderDesc";
+
+//The purpose of this component is to display reviews from customers
+export default function Reviews() {
+  return(
+    <div>
+      <HeaderDesc header="Reviews" description="View some reviews from our patrons:"/>
+    </div>
+  )
+}
