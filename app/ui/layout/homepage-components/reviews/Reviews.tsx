2039fb794bff3d82cd32b8de851f9bc698020982
Lucas Amberg <102396588+lucas-amberg@users.noreply.github.com>
2024-01-09 15:07:02 -0800

    Set up reviews

--- app/ui/layout/homepage-components/reviews/Reviews.tsx
+++ app/ui/layout/homepage-components/reviews/Reviews.tsx
@@ -13,10 +13,10 @@ type ReviewProp = {
 function Review({name, date, reviewContent, rating}: ReviewProp) {
   
   return(
-    <div className="bg-gray-200 w-64 h-64 p-4 shadow-lg rounded-lg flex flex-col">
+    <div className="bg-gray-200 w-64 h-64 p-4 shadow-lg rounded-lg flex gap-2 flex-col">
       <h1 className="text-lg font-bold">{name}</h1>
       <h2>{date}</h2>
-      <p>{`"${reviewContent}"`}</p>
+      <p>{reviewContent === '' ? '' : `"${reviewContent}"`}</p>
       <ReactStars value={rating} count={5} edit={false} size={36}/>
     </div>
   )
@@ -29,8 +29,9 @@ export default function Reviews() {
       <HeaderDesc header="Reviews" description="View some reviews from our patrons:"/>
       <div className="flex flex-wrap gap-12 w-screen p-8 items-center justify-center">
         <Review name="Lucas A." date="Dec 2023" reviewContent="Very great restaurant! Amazing food and service, exceeded our standards. Will be back!" rating={5}/>
-        <Review name="Lucas A." date="Dec 2023" reviewContent="Very great restaurant! Amazing food and service, exceeded our standards. Will be back!" rating={5}/>
-        <Review name="Lucas A." date="Dec 2023" reviewContent="Very great restaurant! Amazing food and service, exceeded our standards. Will be back!" rating={5}/>
+        <Review name="Lauren W." date="Aug 2023" reviewContent="Absolutely great food. Will be back" rating={5}/>
+        <Review name="Phil G." date="Nov 2023" reviewContent="" rating={5}/>
+        <Review name="Dan B." date="Nov 2023" reviewContent="Will come back to Costa Mesa just for this food! Amazing service, pleasure to dine here." rating={5}/>
       </div>
     </div>
   )
